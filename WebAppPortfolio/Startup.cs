45464241e0ca52ec298e3cc988cf841b9eb25abf
Tom Biagioni <tbiagioni1983@gmail.com>
2018-02-10 14:24:03 -0800

    Initial Commit

--- WebAppPortfolio/Startup.cs
+++ WebAppPortfolio/Startup.cs
@@ -20,15 +20,17 @@ namespace WebAppPortfolio
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
