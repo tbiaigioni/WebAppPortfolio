a51ac96df0c08de0d4cae32ca07c59a03d945a05
Tom Biagioni <tbiagioni1983@gmail.com>
2018-02-10 19:26:46 -0800

    Added MVC

---
+++ WebAppPortfolio/Controllers/AppController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppPortfolio.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Me";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Me";
+
+            return View();
+        }
+    }
+}
--- WebAppPortfolio/Startup.cs
+++ WebAppPortfolio/Startup.cs
@@ -15,22 +15,30 @@ namespace WebAppPortfolio
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if(env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
 
-            //app.Run(async (context) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!");
-            //});
+            app.UseStaticFiles();
+
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default"
+                    , "{controller}/{action}/{id?}"
+                    , new { controller = "App", Action = "Index" });
+            });
+
+
         }
     }
 }
