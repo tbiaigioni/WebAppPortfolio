6c8ccb5a4ebead4eb234ef0a61c068757587c43e
Tom Biagioni <tbiagioni1983@gmail.com>
2018-02-12 05:13:14 -0800

    altered shop page

--- WebAppPortfolio/Data/PortfolioUow.cs
+++ WebAppPortfolio/Data/PortfolioUow.cs
@@ -19,6 +19,8 @@ namespace WebAppPortfolio.Data
 
 
         public IProductsRepository Products => GetRepository<IProductsRepository>();
+
+
         protected IRepositoryProvider RepositoryProvider { get; set; }
         private PortfolioContext context;
 
--- WebAppPortfolio/Data/Repositories/ProductsRepository.cs
+++ WebAppPortfolio/Data/Repositories/ProductsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,14 +10,22 @@ namespace WebAppPortfolio.Data
 {
     public class ProductsRepository : EFRepository<Product>,IProductsRepository
     {
-        public ProductsRepository(PortfolioContext dbContext) : base(dbContext)
+        private readonly ILogger<ProductsRepository> logger;
+
+        public ProductsRepository(PortfolioContext dbContext,ILogger<ProductsRepository> logger) : base(dbContext)
         {
+            this.logger = logger;
         }
         
 
         public IQueryable<Product> GetAllProducts()
         {
             return DbContext.Products.OrderBy(p => p.Title);
+        }
+
+        public IQueryable<Product> GetProductsByCategory(string category)
+        {
+            return DbContext.Products.Where(p => p.Category == category);
         }
     }
 }
